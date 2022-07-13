dfd9dd37d25e76075ffc5ec9c87f741cf62122a4
Linxiao Wei <linxiao.wei@rea-group.com>
2022-07-13 21:33:26 +0800

    Initial Commit

---
+++ AnimationExample/AnimationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExampleApp.swift
+//  AnimationExample
+//
+//  Created by WorkInRea on 2022/7/13.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
